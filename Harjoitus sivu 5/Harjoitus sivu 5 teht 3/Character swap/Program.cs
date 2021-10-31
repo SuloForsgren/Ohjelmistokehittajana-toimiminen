3c4099908b0d1a1e758892b6d2d7ff415b61b790
SuloForsgren <sulo.forsgren@gmail.com>
2021-10-31 22:20:10 +0200

    Harjoitukset tähän asti

---
+++ Harjoitus sivu 5/Harjoitus sivu 5 teht 3/Character swap/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Linq;
+using System.Text;
+
+
+namespace Character_swap
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string str1, str2 = "";
+            int i;
+            Console.WriteLine("Anna jokin sana");
+            str1 = Console.ReadLine();
+            string last_char = str1[^1..];
+            string result = str1.Remove(str1.Length-1);
+
+
+            for (i = 1; i < result.Length; i++)
+            {
+                str2 = str2 + result[i];
+            }
+
+            str2 = str2 + result[0];
+
+            string verylast = (last_char + str2);
+            Console.WriteLine("Sekoitettuna sana on: " + verylast);
+
+
+        }
+    }
+}
+
