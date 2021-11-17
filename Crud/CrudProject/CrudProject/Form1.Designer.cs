18c5ae767e3d751e25790541a1a15bcc7f9c9240
SuloForsgren <sulo.forsgren@gmail.com>
2021-11-17 08:53:04 +0200

    Crud tietokanta tehtävä

---
+++ Crud/CrudProject/CrudProject/Form1.Designer.cs
@@ -0,0 +1,274 @@
+﻿
+namespace CrudProject
+{
+    partial class Kirjautumisikkuna
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.IdTB = new System.Windows.Forms.TextBox();
+            this.EtunimiTB = new System.Windows.Forms.TextBox();
+            this.SukunimiTB = new System.Windows.Forms.TextBox();
+            this.PuhelinTB = new System.Windows.Forms.TextBox();
+            this.SpostiTB = new System.Windows.Forms.TextBox();
+            this.OpiskelijanroTB = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.TietolaatikkoDG = new System.Windows.Forms.DataGridView();
+            this.TyhjennysBT = new System.Windows.Forms.Button();
+            this.TallennusBT = new System.Windows.Forms.Button();
+            this.PaivitysBT = new System.Windows.Forms.Button();
+            this.PoistoBT = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.TietolaatikkoDG)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // IdTB
+            // 
+            this.IdTB.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.IdTB.Location = new System.Drawing.Point(423, 273);
+            this.IdTB.Name = "IdTB";
+            this.IdTB.Size = new System.Drawing.Size(100, 31);
+            this.IdTB.TabIndex = 0;
+            // 
+            // EtunimiTB
+            // 
+            this.EtunimiTB.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.EtunimiTB.Location = new System.Drawing.Point(423, 328);
+            this.EtunimiTB.Name = "EtunimiTB";
+            this.EtunimiTB.Size = new System.Drawing.Size(100, 31);
+            this.EtunimiTB.TabIndex = 1;
+            this.EtunimiTB.TextChanged += new System.EventHandler(this.textBox2_TextChanged);
+            // 
+            // SukunimiTB
+            // 
+            this.SukunimiTB.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.SukunimiTB.Location = new System.Drawing.Point(423, 384);
+            this.SukunimiTB.Name = "SukunimiTB";
+            this.SukunimiTB.Size = new System.Drawing.Size(100, 31);
+            this.SukunimiTB.TabIndex = 2;
+            // 
+            // PuhelinTB
+            // 
+            this.PuhelinTB.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.PuhelinTB.Location = new System.Drawing.Point(681, 273);
+            this.PuhelinTB.Name = "PuhelinTB";
+            this.PuhelinTB.Size = new System.Drawing.Size(100, 31);
+            this.PuhelinTB.TabIndex = 3;
+            // 
+            // SpostiTB
+            // 
+            this.SpostiTB.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.SpostiTB.Location = new System.Drawing.Point(681, 328);
+            this.SpostiTB.Name = "SpostiTB";
+            this.SpostiTB.Size = new System.Drawing.Size(100, 31);
+            this.SpostiTB.TabIndex = 4;
+            // 
+            // OpiskelijanroTB
+            // 
+            this.OpiskelijanroTB.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.OpiskelijanroTB.Location = new System.Drawing.Point(681, 384);
+            this.OpiskelijanroTB.Name = "OpiskelijanroTB";
+            this.OpiskelijanroTB.Size = new System.Drawing.Size(100, 31);
+            this.OpiskelijanroTB.TabIndex = 5;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.Lime;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(317, 276);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(32, 25);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "ID";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.BackColor = System.Drawing.Color.Lime;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(538, 387);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(137, 25);
+            this.label2.TabIndex = 7;
+            this.label2.Text = "Opiskelijanro";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.BackColor = System.Drawing.Color.Lime;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(538, 331);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(119, 25);
+            this.label3.TabIndex = 8;
+            this.label3.Text = "Sähköposti";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.BackColor = System.Drawing.Color.Lime;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(538, 276);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(84, 25);
+            this.label4.TabIndex = 9;
+            this.label4.Text = "Puhelin";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.BackColor = System.Drawing.Color.Lime;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(317, 331);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(83, 25);
+            this.label5.TabIndex = 10;
+            this.label5.Text = "Etunimi";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.BackColor = System.Drawing.Color.Lime;
+            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.Location = new System.Drawing.Point(317, 390);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(100, 25);
+            this.label6.TabIndex = 11;
+            this.label6.Text = "Sukunimi";
+            // 
+            // TietolaatikkoDG
+            // 
+            this.TietolaatikkoDG.BackgroundColor = System.Drawing.Color.Red;
+            this.TietolaatikkoDG.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.TietolaatikkoDG.Location = new System.Drawing.Point(275, 12);
+            this.TietolaatikkoDG.Name = "TietolaatikkoDG";
+            this.TietolaatikkoDG.Size = new System.Drawing.Size(577, 238);
+            this.TietolaatikkoDG.TabIndex = 12;
+            this.TietolaatikkoDG.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellContentClick);
+            // 
+            // TyhjennysBT
+            // 
+            this.TyhjennysBT.BackColor = System.Drawing.SystemColors.HighlightText;
+            this.TyhjennysBT.Location = new System.Drawing.Point(316, 448);
+            this.TyhjennysBT.Name = "TyhjennysBT";
+            this.TyhjennysBT.Size = new System.Drawing.Size(101, 35);
+            this.TyhjennysBT.TabIndex = 13;
+            this.TyhjennysBT.Text = "Tyhjennä";
+            this.TyhjennysBT.UseVisualStyleBackColor = false;
+            this.TyhjennysBT.Click += new System.EventHandler(this.TyhjennysBT_Click);
+            // 
+            // TallennusBT
+            // 
+            this.TallennusBT.BackColor = System.Drawing.SystemColors.HighlightText;
+            this.TallennusBT.Location = new System.Drawing.Point(439, 448);
+            this.TallennusBT.Name = "TallennusBT";
+            this.TallennusBT.Size = new System.Drawing.Size(95, 35);
+            this.TallennusBT.TabIndex = 14;
+            this.TallennusBT.Text = "Tallenna";
+            this.TallennusBT.UseVisualStyleBackColor = false;
+            this.TallennusBT.Click += new System.EventHandler(this.TallennusBT_Click);
+            // 
+            // PaivitysBT
+            // 
+            this.PaivitysBT.BackColor = System.Drawing.SystemColors.HighlightText;
+            this.PaivitysBT.Location = new System.Drawing.Point(557, 448);
+            this.PaivitysBT.Name = "PaivitysBT";
+            this.PaivitysBT.Size = new System.Drawing.Size(100, 35);
+            this.PaivitysBT.TabIndex = 15;
+            this.PaivitysBT.Text = "Päivitä";
+            this.PaivitysBT.UseVisualStyleBackColor = false;
+            this.PaivitysBT.Click += new System.EventHandler(this.PaivitysBT_Click_1);
+            // 
+            // PoistoBT
+            // 
+            this.PoistoBT.BackColor = System.Drawing.SystemColors.HighlightText;
+            this.PoistoBT.Location = new System.Drawing.Point(681, 448);
+            this.PoistoBT.Name = "PoistoBT";
+            this.PoistoBT.Size = new System.Drawing.Size(100, 35);
+            this.PoistoBT.TabIndex = 16;
+            this.PoistoBT.Text = "Poista";
+            this.PoistoBT.UseVisualStyleBackColor = false;
+            this.PoistoBT.Click += new System.EventHandler(this.PoistoBT_Click);
+            // 
+            // Kirjautumisikkuna
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.MenuHighlight;
+            this.ClientSize = new System.Drawing.Size(1063, 495);
+            this.Controls.Add(this.PoistoBT);
+            this.Controls.Add(this.PaivitysBT);
+            this.Controls.Add(this.TallennusBT);
+            this.Controls.Add(this.TyhjennysBT);
+            this.Controls.Add(this.TietolaatikkoDG);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.OpiskelijanroTB);
+            this.Controls.Add(this.SpostiTB);
+            this.Controls.Add(this.PuhelinTB);
+            this.Controls.Add(this.SukunimiTB);
+            this.Controls.Add(this.EtunimiTB);
+            this.Controls.Add(this.IdTB);
+            this.Name = "Kirjautumisikkuna";
+            this.Text = "Tietokanta ikkuna";
+            this.Load += new System.EventHandler(this.Kirjautumisikkuna_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.TietolaatikkoDG)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox IdTB;
+        private System.Windows.Forms.TextBox EtunimiTB;
+        private System.Windows.Forms.TextBox SukunimiTB;
+        private System.Windows.Forms.TextBox PuhelinTB;
+        private System.Windows.Forms.TextBox SpostiTB;
+        private System.Windows.Forms.TextBox OpiskelijanroTB;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.DataGridView TietolaatikkoDG;
+        private System.Windows.Forms.Button TyhjennysBT;
+        private System.Windows.Forms.Button TallennusBT;
+        private System.Windows.Forms.Button PaivitysBT;
+        private System.Windows.Forms.Button PoistoBT;
+    }
+}
+
